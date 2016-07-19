792fa7581120f96a8d2204c4eb7eb6891630483c
Gerard Wilson <lucid7net@gmail.com>
2016-07-19 10:18:14 +1000

    10am Tuesday

--- main.js
+++ main.js
@@ -71,7 +71,7 @@
             `<li><a href="#">${source.name}</a></li>`
           )
   }
-  renderHeader(state, header);
+
 
   function renderContainer (state, into){
          into.innerHTML = `
@@ -88,14 +88,14 @@
         `
         <article class="article">
         <section class="featured-image">
-          <img src="images/article_placeholder_1.jpg" alt="" />
+          <img src="${article.img}" alt="" />
         </section>
         <section class="article-content">
-          <a href="#"><h3>article.title</h3></a>
-          <h6>Lifestyle</h6>
+          <a href="#"><h3>${article.title}</h3></a>
+          <h6>${article.category}</h6>
         </section>
         <section class="impressions">
-          526
+          ${article.impressions}
         </section>
         <div class="clearfix"></div>
       </article>
@@ -105,7 +105,75 @@
   }
 
 
- <section id="main" class="wrapper">
-      
-      
-    </section>
+ function fetchRedditData(){
+    fetch('https://www.reddit.com/top.json')
+      .then((response) => {
+        return response.json()
+      }).then((result) => {
+        result.data.children.forEach((item) => {
+          var article = {}
+          article.title =  item.data.title,
+          article.img = item.data.thumbnail,
+          article.url =  item.data.url,
+          article.impressions =  item.data.ups,
+          article.category =  'Latest from Reddit',
+          article.description =  item.data.title
+          state.articles.push(article);
+        })
+        renderContainer(state, container);
+      })  
+ }
+
+ fetchRedditData();
+
+
+
+     
+
+function fetchMashableData(){
+  fetch('https://crossorigin.me/http://mashable.com/stories.json')
+  .then((response) => {
+    return response.json()
+  }).then((result) => {
+    result.new.forEach((item) => {
+      var article = {}
+          article.title =  item.display_title,
+          article.img = item.image,
+          article.url =  item.link,
+          article.impressions =  item.shares.total,
+          article.category =  item.channel,
+          article.description =  item.excerpt
+          state.articles.push(article);
+        })
+        renderContainer(state, container);
+      })  
+ }
+
+fetchMashableData();
+
+
+function fetchGuardianData(){
+  fetch('https://crossorigin.me/http://content.guardianapis.com/au?show-editors-picks=true&api-key=622f5036-10b9-47a4-9f76-2b043582aaaf')
+  .then((response) => {
+    return response.json()
+  }).then((result) => {
+    result.response.editorsPicks.forEach((item) => {
+     var article = {}
+          article.title =  item.webTitle,
+          article.img = item.image,
+          article.url =  item.webUrl,
+          article.impressions =  'unavailable'//item.shares.total,
+          article.category =  item.sectionName,
+          article.description =  item.apiUrl
+          state.articles.push(article);
+    })
+  })}
+  fetchGuardianData();
+  
+
+
+renderHeader(state, header);
+
+    /*delegate('header','click','.news-source', handleFilterClick)
+  	delegate('#container','click','.article', handleArticleClick)
+	  delegate('#container','click','.close-pop-up', handleClosePopUpClick)*/
